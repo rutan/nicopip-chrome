3ef9888517919daf43dfb83f6f11295c55755b81
i4M1k0SU <i4m1k0su@i4m1k0.su>
2020-04-16 21:49:37 +0900

    add unofficial text

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,7 +2,7 @@ import { copyButton } from './functions/copyButton';
 import { initPinP, startPinP } from './functions/pinp';
 import PictureInPictureIcon from './svg/pinpIcon';
 
-const TITLE = 'Picture in Picture';
+const TITLE = '[非公式] Picture in Picture';
 
 const fullScreenButton = document.querySelector(
   '.ControllerContainer .EnableFullScreenButton, [class^=___addon-controller___] [class^=___fullscreen-button___]'
