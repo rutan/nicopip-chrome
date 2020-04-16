d51a75af82874918e671520f888d46f03bc78bd3
ru_shalm <ru_shalm@hazimu.com>
2020-04-16 22:26:32 +0900

    short name
    ニコニコ動画プレイヤーのツールチップは
    overflow: hiddenが指定されており、長いと文字が見切れてしまうため

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,7 +2,7 @@ import { copyButton } from './functions/copyButton';
 import { initPinP, startPinP } from './functions/pinp';
 import PictureInPictureIcon from './svg/pinpIcon';
 
-const TITLE = '[非公式] Picture in Picture';
+const TITLE = '[非公式] PinP';
 
 const fullScreenButton = document.querySelector(
   '.ControllerContainer .EnableFullScreenButton, [class^=___addon-controller___] [class^=___fullscreen-button___]'
