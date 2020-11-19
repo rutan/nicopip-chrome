71d24aae5ca571fedf73da3cfbac6163fcf92c0d
AyumuNekozuki <ayumu@nekozuki.me>
2020-11-19 12:25:22 +0900

    jsを遅延動作させるように

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,14 +2,16 @@ import { copyButton } from './functions/copyButton';
 import { initPinP, startPinP } from './functions/pinp';
 import PictureInPictureIcon from './svg/pinpIcon';
 
-const TITLE = '[非公式] PinP';
+window.onload = function() {
+  const TITLE = '[非公式] PinP';
 
-const fullScreenButton = document.querySelector(
-  '.ControllerContainer .EnableFullScreenButton, [class^=___addon-controller___] [class^=___fullscreen-button___]'
-);
+  const fullScreenButton = document.querySelector(
+    '.ControllerContainer .EnableFullScreenButton, [class^=___addon-controller___] [class^=___fullscreen-button___]'
+  );
 
-if (fullScreenButton) {
-  initPinP();
-  // フルスクボタンをコピーしPinPボタンにする
-  copyButton(fullScreenButton, TITLE, PictureInPictureIcon, startPinP);
-}
+  if (fullScreenButton) {
+    initPinP();
+    // フルスクボタンをコピーしPinPボタンにする
+    copyButton(fullScreenButton, TITLE, PictureInPictureIcon, startPinP);
+  }
+};
